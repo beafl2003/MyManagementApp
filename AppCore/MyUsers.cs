73db3df8ff346cf064ae728ed9b1888d166e6bd5
Anna Beatriz Azevedo Fidelis <109418073+beafl2003@users.noreply.github.com>
2022-08-31 18:01:41 -0300

    Ajuste da AppCore

---
+++ AppCore/MyUsers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MyUsers
+
+
+{
+    public class user
+    {
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+    }
+
+   
+
+  
+}
+
