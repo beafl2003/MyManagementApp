1dd365c037337dc7d1bd389000ffe87788a4007b
Anna Beatriz Azevedo Fidelis <109418073+beafl2003@users.noreply.github.com>
2022-11-18 14:20:39 -0300

    Project Update - 17112022

---
+++ src/MyManagementApp.Domain/Order.cs
@@ -0,0 +1,17 @@
+﻿using MyManagementApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyManagementApp.Domain.Core
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public string CustomerCode { get; set; }
+        public string CustomerName { get; set; }
+        public OrderStatusEnum OrderStatus { get; set; }
+    }
+}
