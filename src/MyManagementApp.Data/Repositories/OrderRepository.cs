583c5ab7b228c97a2a178e2ddd9b9f458c695530
Anna <anna.fidelis@dataplace.com.br>
2023-02-14 18:01:52 -0300

    021423 - Find order by Customer Repository Start

--- src/MyManagementApp.Data/Repositories/OrderRepository.cs
+++ src/MyManagementApp.Data/Repositories/OrderRepository.cs
@@ -73,6 +73,62 @@ public class OrderRepository
 
     }
 
+    public Order GetOrdersByCustomer(int customerid)
+    {
+        var dbConnection = ConnectionProvider.GetConnection();
+        var sql = $@"
+                SELECT  OrderNumber,
+		                OrderStatus,
+		                Customerid,
+		                Customers.code as 'Customers.code',
+		                Customers.name as 'Customers.name',
+		                Customers.active as 'CustomerStatus'
+		        FROM Orders
+                INNER JOIN Customers ON 
+	                     Customers.id = Orders.Customerid 
+	                AND  Customers.id = Orders.Customerid
+	               WHERE  Customerid = @Customerid";
+
+
+        var command = new SqlCommand(sql, dbConnection);
+        command.Parameters.Add(new SqlParameter("@Customerid", customerid));
+
+        var adapter = new SqlDataAdapter(command);
+        // dataTable
+        var table = new DataTable();
+        adapter.Fill(table);
+
+
+        if (table.Rows.Count == 0)
+            return default;
+
+        // datarow
+        var row = table.Rows[0];
+
+        var StatusOfTheOrder = row.Field<string>("OrderStatus");
+
+        var Order = new Order()
+        {
+            OrderNumber = row.Field<int>("OrderNumber"),
+            CustomerID = row.Field<Guid>("Customerid"),
+            OrderStatus = (OrderStatusEnum)StatusOfTheOrder.ToOrderStatusEnum(),
+            //OrderStatus = row.Field<OrderStatusEnum>((int)("OrderStatus").ToOrderStatusEnum())
+            //cbxCustomerStatus.SelectedItem.ToString()));
+
+            CustomerCode = row.Field<string>("Customers.code"),
+            CustomerName = row.Field<string>("Customers.name"),
+
+        };
+
+        if (dbConnection.State == ConnectionState.Open)
+            dbConnection.Close();
+
+        // Clean RAM 
+        dbConnection.Dispose();
+        dbConnection = null;
+
+        return Order;
+    }
     public bool InsertDatabase(Order order)
     {
         var dbConnection = ConnectionProvider.GetConnection();
